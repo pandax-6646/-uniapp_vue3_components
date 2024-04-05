65da81c4fca3d7ed7d1de3e8c47609cc566da8d0
pandax-6646 <65929759+pandax-6646@users.noreply.github.com>
2024-04-05 12:50:59 +0800

    chore: 升级更加严格的代码提交规范

--- .cz-config.js
+++ .cz-config.js
@@ -5,16 +5,16 @@ module.exports = {
     { value: 'docs', name: 'docs: 文档、注释变更' },
     { value: 'style', name: 'style: 代码格式（不影响功能，例如空格、分号等格式修正）' },
     { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'perf', name: 'perf: 性能优化' },
     { value: 'test', name: 'test: 添加疏漏测试或已有测试改动' },
     { value: 'build', name: 'build: 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
-    { value: 'ci', name: 'ci: 修改 CI 配置、脚本' },
+    { value: 'ci', name: 'ci: 修改项目继续集成流程（Travis，Jenkins，GitLab CI，Circle等）' },
     { value: 'chore', name: 'chore: 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    {
-      value: 'improvement',
-      name: 'improvement: 用于对当前实现进行改进而没有添加新功能或修复错误的提交',
-    },
+    { value: 'revert', name: 'revert: 代码回退' },
+
     { value: 'merge', name: 'merge: 仅进行分支合并' },
-    { value: 'revert', name: 'revert: 回滚到上一个commmit' },
+    { value: 'wip', name: 'revert: 开发中' },
+    { value: 'refine', name: 'revert: 小优化，没有到 refactor 的程度' }
   ],
 
   messages: {
@@ -26,10 +26,10 @@ module.exports = {
     body: '长描述，使用"|"换行(可选):\n',
     breaking: '非兼容性说明 (可选):\n',
     footer: '关联关闭的issue:例如:#1, #2(可选):\n',
-    confirmCommit: '确定提交?',
+    confirmCommit: '确定提交?'
   },
 
   allowBreakingChanges: ['feat', 'fix'],
   skipQuestions: ['scope','customScope', 'body', 'breaking', 'footer'],
-  subjectLimit: 100,
+  subjectLimit: 100
 }
